1efe99f88ec4a94b2d60c00aed61f596f8e8f08f
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-05-21 15:03:21 +0530

    Added Little Lemon Header and Footer

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+// Importing Local Components
+import LittleLemonHeader from './components/LittleLemonHeader';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Samyak Jain !! This is your first React Native App</Text>
-      <Text>Lets go!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={{
+        flex: 1,
+        backgroundColor: '#495E57'
+      }}>
+        <LittleLemonHeader />
+      </View >
+
+      <View style={{ backgroundColor: '#495E57' }}>
+        <Footer />
+      </View>
+
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/LittleLemonHeader.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+export default function LittleLemonHeader() {
+    return (
+        <View style={{ flex: 0.17, backgroundColor: '#F4CE14' }}>
+            <Text style={{
+                paddingTop: 63,
+                textAlign: 'center',
+                fontSize: 30,
+                color: 'black',
+                fontWeight: 'bold',
+            }}>Little Lemon</Text>
+
+            <Text style={{ textAlign: 'center', fontStyle: 'italic', fontSize: 17, }}>-- By Samyak Jain</Text>
+        </View>
+    )
+}

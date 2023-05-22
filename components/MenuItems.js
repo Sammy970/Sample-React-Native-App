a4772b374144b27450cef5d3917c81eeb1e0bff9
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-05-22 23:09:35 +0530

    Added more features to FlatList in MenuItems

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -5,47 +5,56 @@ const green = '#495E57';
 const yellow = '#F4CE14';
 
 const menuItemsToDisplay = [
-    { name: 'Hummus', id: '1A' },
-    { name: 'Moutabal', id: '2B' },
-    { name: 'Falafel', id: '3C' },
-    { name: 'Marinated Olives', id: '4D' },
-    { name: 'Kofta', id: '5E' },
-    { name: 'Eggplant Salad', id: '6F' },
-    { name: 'Lentil Burger', id: '7G' },
-    { name: 'Smoked Salmon', id: '8H' },
-    { name: 'Kofta Burger', id: '9I' },
-    { name: 'Turkish Kebab', id: '10J' },
-    { name: 'Fries', id: '11K' },
-    { name: 'Buttered Rice', id: '12L' },
-    { name: 'Bread Sticks', id: '13M' },
-    { name: 'Pita Pocket', id: '14N' },
-    { name: 'Lentil Soup', id: '15O' },
-    { name: 'Greek Salad', id: '16Q' },
-    { name: 'Rice Pilaf', id: '17R' },
-    { name: 'Baklava', id: '18S' },
-    { name: 'Tartufo', id: '19T' },
-    { name: 'Tartufo', id: '20U' },
-    { name: 'Tiramisu', id: '21V' },
-    { name: 'Panna Cotta', id: '22W' },
+    { name: 'Hummus', price: '$5.00', id: '1A' },
+    { name: 'Moutabal', price: '$5.00', id: '2B' },
+    { name: 'Falafel', price: '$7.50', id: '3C' },
+    { name: 'Marinated Olives', price: '$5.00', id: '4D' },
+    { name: 'Kofta', price: '$5.00', id: '5E' },
+    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+    { name: 'Lentil Burger', price: '$10.00', id: '7G' },
+    { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
+    { name: 'Kofta Burger', price: '$11.00', id: '9I' },
+    { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
+    { name: 'Fries', price: '$3.00', id: '11K' },
+    { name: 'Buttered Rice', price: '$3.00', id: '12L' },
+    { name: 'Bread Sticks', price: '$3.00', id: '13M' },
+    { name: 'Pita Pocket', price: '$3.00', id: '14N' },
+    { name: 'Lentil Soup', price: '$3.75', id: '15O' },
+    { name: 'Greek Salad', price: '$6.00', id: '16Q' },
+    { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
+    { name: 'Baklava', price: '$3.00', id: '18S' },
+    { name: 'Tartufo', price: '$3.00', id: '19T' },
+    { name: 'Tiramisu', price: '$5.00', id: '20U' },
+    { name: 'Panna Cotta', price: '$5.00', id: '21V' },
 ];
 
-const Item = ({ name }) => (
+const Seperator = () => <View style={menuStyles.seperator} />
+
+const Header = () => <Text style={menuStyles.headerText}>View Menu</Text>
+
+const Footer = () => <Text style={menuStyles.headerText}>--  End  --</Text>
+
+const Item = ({ name, price }) => (
     <View style={menuStyles.innerContainer}>
         <Text style={menuStyles.itemText}>{name}</Text>
+        <Text style={menuStyles.itemText}>{price}</Text>
     </View>
 )
 
 const MenuItems = () => {
-    const renderItem = ({ item }) => <Item name={item.name} />;
+    const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
     return (
 
-        <View style={menuStyles.innerContainer}>
-            <Text style={menuStyles.headerText}>View Menu</Text>
+        <View style={menuStyles.container}>
             <FlatList
                 data={menuItemsToDisplay}
                 keyExtractor={(item) => item.id}
-                renderItem={renderItem}></FlatList>
+                renderItem={renderItem}
+                ItemSeparatorComponent={Seperator}
+                ListHeaderComponent={Header}
+                ListFooterComponent={Footer}
+            />
         </View>
 
     );
@@ -56,17 +65,27 @@ const menuStyles = StyleSheet.create({
         flex: 1
     },
     innerContainer: {
-        padding: 40,
+        paddingHorizontal: 45,
+        paddingVertical: 17,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         backgroundColor: green
+        // paddingLeft: 35,
     },
     headerText: {
-        fontSize: 40,
+        paddingVertical: 20,
+        fontSize: 30,
         flexWrap: 'wrap',
-        color: 'white'
+        color: 'white',
+        textAlign: 'center'
     },
     itemText: {
         color: yellow,
-        fontSize: 25
+        fontSize: 20
+    },
+    seperator: {
+        borderBottomWidth: 2,
+        borderColor: 'white'
     }
 })
 
